d22d59ea0bea4830799fa5873855d7834b487bd6
Mael <lucas47772005@gmail.com>
2025-06-06 18:00:57 +0200

    Classe connect implémentée, début de login

---
+++ first-dashboard/auth.py
@@ -0,0 +1,22 @@
+
+
+from shiny import App, ui, render
+
+
+
+auth_ui = ui.page_fluid(
+        ui.h1('Shinyboard'),
+        ui.h2('Veuillez vous authentifier pour accéder à vôtre application.'),
+
+        ui.input_text(
+            'res_users_login',
+            ui.span("Login"),
+            placeholder="login"
+        )
+    )
+
+
+def auth_server(input, output, session):
+    pass
+
+auth_app = App(auth_ui, auth_server)
--- first-dashboard/sales/sale_data.py
+++ first-dashboard/sales/sale_data.py
@@ -1,20 +1,33 @@
 import polars as pl
 import connectorx as cx
 from models.sales_data_model import sales_data_model
+from connect import Connect
+
+"""TODO
+- Mettre 'db' dans un singleton, ou faire un Design en Factory pour empêcher lles répétition de
+db = Connect("dsn")
+"""
 
 # CX Params
 
-
 def getSalesData():
-    connection = "postgresql://odoo:odoodb@127.0.0.1:5432/test"
     #df_type = "polars"
+
+    db = Connect("dsn1")
 
     sales_model = sales_data_model()
     
     # primary tables
-    sales_model.sale_order = cx.read_sql(conn=connection, query="SELECT * FROM sale_order", return_type='polars').rename({"id": "sale_order_id", "user_id": "sale_order_uid"})
-    sales_model.res_users = cx.read_sql(conn=connection, query="SELECT * FROM res_users", return_type='polars').rename({"id": "res_users_id", "login": "res_users_login"})
-    sales_model.res_partner = cx.read_sql(conn=connection, query="SELECT * FROM res_partner", return_type='polars').rename({"id": "res_partner_id", "user_id": "res_partner_uid", "name": "res_partner_name"})
+    sales_model.sale_order = db.read("SELECT * FROM sale_order").rename({
+        "user_id": "sale_order_uid"
+    })
+
+    sales_model.res_users = db.read("SELECT * FROM res_users")
+
+    sales_model.res_partner = db.read("SELECT * FROM res_partner").rename({
+        "user_id" : "res_partner_uid",
+        "name" : "res_partner_name"
+    })
 
     # joins
     sales_model.salesperson_amount_no_tax = (
@@ -30,8 +43,8 @@ def getSalesData():
     )
 
     # dates and times
-    sales_model.date_df = cx.read_sql(conn=connection, query="SELECT CURRENT_TIMESTAMP(0) + interval '2 hours' AS current_time, DATE_TRUNC('month', CURRENT_TIMESTAMP(0)) AS truncated ", return_type='polars')
-    sales_model.relative_dates_df = cx.read_sql(conn=connection, query="SELECT MAX(date_order) + interval '1 days' AS max_do, MIN(date_order) AS min_do FROM sale_order",return_type='polars')
+    sales_model.date_df = db.read("SELECT CURRENT_TIMESTAMP(0) + interval '2 hours' AS current_time, DATE_TRUNC('month', CURRENT_TIMESTAMP(0)) AS truncated ")
+    sales_model.relative_dates_df = db.read("SELECT MAX(date_order) + interval '1 days' AS max_do, MIN(date_order) AS min_do FROM sale_order")
     sales_model.current_date = (sales_model.date_df.select("current_time").to_series().dt.replace_time_zone(None).to_list()[0])
 
     sales_model.min_date_order = sales_model.relative_dates_df.select("min_do").to_series().to_list()[0]

27986883fe6f5102d498b19383962b0eba19d6da
David Beal <david.beal@akretion.com>
2025-06-06 08:41:59 +0200

    add connect class

--- first-dashboard/connect.py
+++ first-dashboard/connect.py
@@ -7,27 +7,39 @@ logger = logging.getLogger(__name__)
 dsns = os.getenv("DSN")
 
 
-def get_data_sources():
-    dsn = {}
-    for envdsn in dsns.split("|"):
-        envdsn = envdsn.strip()
-        if not envdsn or ":" not in envdsn:
-            continue
-        dsn_name, dsn_url = dsn.split(":", 1)
-        dsn[dsn_name.strip()] = dsn_url.strip()
-        logger.info(f"DSN {dsn_name} available")
-    return dsn
 
+class Connect():
+    """ my = Connect("mydsn")
+        my.read("SELECT name FROM mytable")
+        
+    """
 
-def get_data_source(mydsn):
-    dsn_url = get_data_sources.get(mydsn)
-    try:
-        connection = cx.read_sql(conn=dsn_url, query="SELECT 1", return_type="polars")
-        # connection.close()
-    except Exception as e:
-        print(f"Failed to connect to {dsn_url}: {e}")
-    return connection
+    conn = False
 
+    def __init__(self, dsn):
+        dsns = self._get_data_sources()
+        if dsn not in dsns:
+            raise Exception("DSN not found in available data sources.")
+        self._get_conn(dsns[dsn])
 
-def _read_sql(self, query, return_type="polars"):
-    return self.db_conf_id._read_sql(query, return_type=return_type)
+    def _get_data_sources(self):
+        dsn = {}
+        for dsn_env in dsns.split("|"):
+            dsn_env = dsn_env.strip()
+            if not dsn_env or "=" not in dsn_env:
+                continue
+            dsn_name, dsn_url = dsn.split("=", 1)
+            dsn[dsn_name.strip()] = dsn_url.strip()
+            logger.info(f"DSN {dsn_name} available")
+        return dsn
+
+    def _get_conn(self, dsn):
+        try:
+            connection = cx.read_sql(conn=dsn, query="SELECT 1", return_type="polars")
+        except Exception as e:
+            print(f"Failed to connect to {dsn}: {e}")
+            raise(e)
+        self.conn = connection
+
+    def read(self, query, return_type="polars"):
+        return cx.read_sql(self.conn, query, return_type=return_type)

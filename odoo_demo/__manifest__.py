35a974defe0bad9a8f3d3487f3060b0a9457e565
David Beal <david.beal@akretion.com>
2025-07-12 10:38:26 +0200

    ADD odoo_demo module

---
+++ odoo_demo/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "Custom Demo Data",
+    "summary": "Data such as partners, etc.",
+    "version": "18.0.1.0.0",
+    "author": "Akretion",
+    "development_status": "Beta",
+    "category": "Sales",
+    "website": "https://github.com/shinyboard",
+    "license": "AGPL-3",
+    "depends": [
+        "sale",
+    ],
+    "data": [
+        "data/res_partner.csv",
+    ],
+    "installable": True,
+}
